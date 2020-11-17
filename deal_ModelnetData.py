a358219dcda8311c75eda4b31ea0b8bf7dc115fb
Blue-Giant <42233512+Blue-Giant@users.noreply.github.com>
2020-11-17 14:55:00 +0800

    Add files via upload

--- deal_ModelnetData.py
+++ deal_ModelnetData.py
@@ -12,11 +12,36 @@ def write_data2h5file(file_name, h5data, label=None):
     h5f.close()
 
 
+# Write numpy array data and label and numpy array normal to h5_filename
+def save_h5_data_label_normal(h5_filename, data, label, normal,
+                              data_dtype='float32', label_dtype='uint8', normal_dtype='float32'):
+    h5_fout = h5py.File(h5_filename)
+    h5_fout.create_dataset(
+            'data', data=data,
+            compression='gzip', compression_opts=4,
+            dtype=data_dtype)
+    h5_fout.create_dataset(
+            'normal', data=normal,
+            compression='gzip', compression_opts=4,
+            dtype=normal_dtype)
+    h5_fout.create_dataset(
+            'label', data=label,
+            compression='gzip', compression_opts=1,
+            dtype=label_dtype)
+    h5_fout.close()
+
+
 # Write numpy array data and label to h5_filename
-def save_h5_data_label(h5_filename, data, label=None, data_dtype='float32', label_dtype='uint8'):
-    h5_fout = h5py.File(h5_filename, 'w')
-    h5_fout.create_dataset('data', data=data, compression='gzip', compression_opts=4, dtype=data_dtype)
-    h5_fout.create_dataset('label', data=label, compression='gzip', compression_opts=1, dtype=label_dtype)
+def save_h5_data_label(h5_filename, data, label, data_dtype='float32', label_dtype='uint8'):
+    h5_fout = h5py.File(h5_filename)
+    h5_fout.create_dataset(
+            'data', data=data,
+            compression='gzip', compression_opts=4,
+            dtype=data_dtype)
+    h5_fout.create_dataset(
+            'label', data=label,
+            compression='gzip', compression_opts=1,
+            dtype=label_dtype)
     h5_fout.close()
 
 
@@ -27,25 +52,105 @@ def getFiles_name(file_dir):
     return files_name
 
 
-# Sampling the points by farthest_point method
-def farthest_point_sample(xyz, npoint):
-    N, C = xyz.shape
-    centroids = np.zeros(npoint)
-    distance = np.ones(N) * 1e10
-    farthest = np.random.randint(0, N)  # random select one as centroid
+# Sampling the points by random method, the return the sampling points
+def random_point_sample(point, npoint):
+    """
+    Input:
+        xyz: pointcloud data, [N, D]
+        npoint: number of samples
+    Return:
+        samplePoint: sampled points from the current cloud, [npoint, D]
+    """
+    N, D = point.shape
+    xyz = point[:, :3]
+    flagArray = np.concatenate((np.ones(npoint), np.zeros(N-npoint)), axis=-1)
+    np.random.shuffle(flagArray)
+    indexes2Points = np.arange(1, N+1, 1, np.int32)
+    flag2Points = np.multiply(flagArray.astype(np.int32), indexes2Points)
+    indexes2samplePoints = []
+    for index in range(N):
+        if flag2Points[index] != 0:
+            indexes2samplePoints.append(flag2Points[index])
+    indexes2samples = np.array(indexes2samplePoints) - 1
+    samplePoint = xyz[indexes2samples.astype(np.int32)]
+    return samplePoint
+
+
+# Sampling the points by uniform method, the return the sampling points
+def uniform_point_sample(point, npoint):
+    """
+    Input:
+        xyz: pointcloud data, [N, D]
+        npoint: number of samples
+    Return:
+        samplePoint: sampled points from the current cloud, [npoint, D]
+    """
+    N, D = point.shape
+    xyz = point[:, :3]
+    indexes2samplePoints = np.zeros((npoint,))
+    distance = np.ones((N,)) * 1e10
+    farthest = np.random.randint(0, N)
     for i in range(npoint):
-        centroids[i] = farthest
-        centroid = xyz[farthest, :].reshape(1, 3)
+        indexes2samplePoints[i] = farthest
+        centroid = xyz[farthest, :]
         dist = np.sum((xyz - centroid) ** 2, -1)
         mask = dist < distance
         distance[mask] = dist[mask]
-        farthest = np.argmax(distance)  # select the farthest one as centroid
-        # print('index:%d, dis:%.3f'%(farthest,np.max(distance)))
-    return centroids
+        farthest = np.argmax(distance, -1)
+    samplePoint = point[indexes2samplePoints.astype(np.int32)]
+    return samplePoint
 
 
-# Read the data from *.off file
-def read_off(filename):
+# Sampling the points by farthest_point method, the return the sampling points
+def farthest_point_sample(point, npoint):
+    """
+    Input:
+        xyz: pointcloud data, [N, D]
+        npoint: number of samples
+    Return:
+        samplePoint: sampled points from the current cloud, [npoint, D]
+    """
+    N, D = point.shape
+    xyz = point[:, :3]
+    indexes2samplePoints = np.zeros((npoint,))
+    distance = np.ones((N,)) * 1e10
+    farthest = np.random.randint(0, N)
+    for i in range(npoint):
+        indexes2samplePoints[i] = farthest
+        centroid = xyz[farthest, :]
+        dist = np.sum((xyz - centroid) ** 2, -1)
+        mask = dist < distance
+        distance[mask] = dist[mask]
+        farthest = np.argmax(distance, -1)
+    samplePoint = point[indexes2samplePoints.astype(np.int32)]
+    return samplePoint
+
+
+# Sampling the points by farthest_point method, the return the indexes of sampling points
+def getIndexes_farthest_point_sample(xyz, npoint):
+    """
+        Input:
+            xyz: pointcloud data, [N, D]
+            npoint: number of samples
+        Return:
+             indexes2samplePoints
+        """
+    N, C = xyz.shape
+    indexes2samplePoints = np.zeros(npoint)
+    distance = np.ones(N) * 1e10                         # initiate the farthest distance array
+    index2farthest = np.random.randint(0, N)             # random select one as initial centroid
+    for i in range(npoint):
+        indexes2samplePoints[i] = index2farthest         # add the index of farthest points into indexes
+        centroid = xyz[index2farthest, :].reshape(1, 3)  # obtain the coords for centroid
+        dist = np.sum((xyz - centroid) ** 2, -1)         # calculating the distance between centroid and other points
+        mask = dist < distance
+        distance[mask] = dist[mask]                      # update the farthest distance array by current results
+        index2farthest = np.argmax(distance)             # select the farthest one as new centroid
+    return indexes2samplePoints
+
+
+# Get the data from *.off file
+def getData_from_Off(filename, opt2sample=None, normalize_PointSet=False):
     f = open(filename)
     f.readline()         # ignore the 'OFF' at the first line
     f.readline()         # ignore the second line
@@ -62,25 +167,31 @@ def read_off(filename):
     f.close()
 
     num2points = 2048  # we need sample 2048 points from current point cloud
-    # if the numbers of points are less than 2000, extent the point set
-    if len(coord2points) < (num2points + 3):
+    # if the numbers of points are less than 2048, extent the point set
+    if len(coord2points) < num2points:
         # print("none")
         return None
     coord2points = np.array(coord2points)
-    # take and shuffle points
-    # index = np.random.choice(len(All_points), num_select, replace=False)
     xyz = coord2points.copy()
-    points_farthest_index = farthest_point_sample(xyz, num2points).astype(np.int64)
-    points_farthest = xyz[points_farthest_index, :]
-    centroid = np.mean(points_farthest, axis=0)
-    points_unit_sphere = points_farthest - centroid
-    furthest_distance = np.max(np.sqrt(np.sum(abs(points_farthest) ** 2, axis=-1)))
-    points_unit_sphere /= furthest_distance
-    # print('*************************')
-    return list(points_unit_sphere)           # return N*3 array 的list
+    if 'farthest_point_sample'== str.lower(opt2sample):
+        points_sample = farthest_point_sample(xyz, num2points).astype(np.int64)
+    elif 'random_point_sample'== str.lower(opt2sample):
+        points_sample = random_point_sample(xyz, num2points)
+    else:
+        points_sample = uniform_point_sample(xyz, num2points)
+    print('points_sample:', points_sample)
+
+    centroid = np.mean(points_sample, axis=0)
+    points_unit_sphere = points_sample - centroid
+    if (normalize_PointSet):
+        # centroid = np.mean(points_sample, axis=0)
+        # points_unit_sphere = points_sample - centroid
+        furthest_distance = np.max(np.sqrt(np.sum(abs(points_unit_sphere) ** 2, axis=-1)))
+        points_unit_sphere /= furthest_distance
+    return points_unit_sphere
 
 
-def make_trian_Dataset(name2originData=None):
+def make_trian_Dataset(name2originData=None, sample_opt=None):
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     print('Base_dir:', BASE_DIR)
     print('\n')
@@ -114,7 +225,7 @@ def make_trian_Dataset(name2originData=None):
             data_filename = os.path.join(data_dir, fileName)
             # data_filename = os.path.join(data_dir, fileName)
             print(data_filename)
-            current_data = read_off(data_filename)
+            current_data = getData_from_Off(data_filename, opt2sample=sample_opt, normalize_PointSet=True)
             if current_data == None:
                 continue
             label_list2class.append(current_label)
@@ -142,7 +253,7 @@ def make_trian_Dataset(name2originData=None):
             label_list2class.clear()
 
 
-def make_test_Dataset(name2originData=None):
+def make_test_Dataset(name2originData=None, sample_opt=None):
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     print('Base_dir:', BASE_DIR)
     print('\n')
@@ -173,7 +284,7 @@ def make_test_Dataset(name2originData=None):
                 continue
             data_filename = os.path.join(data_dir, fileName)
             print(data_filename)
-            current_data = read_off(data_filename)
+            current_data = getData_from_Off(data_filename, opt2sample=sample_opt, normalize_PointSet=True)
             if current_data == None:
                 continue
             label_list2class.append(current_label)
@@ -194,9 +305,11 @@ if __name__ == "__main__":
     # # getFiles_name(filePath)
     # filename = 'data/ModelNet10/bathtub/train/bathtub_0001.off'
     # # filename = 'bathtub_0001.off'
-    # data = read_off(filename)
+    # data2points = getData_from_Off(filename, normalize_PointSet=False)
+    # print('data2points:', data2points)
 
     dataName = 'ModelNet10'
-    make_trian_Dataset(name2originData=dataName)
-    make_test_Dataset(name2originData=dataName)
+    option2sample = 'random_point_sample'
+    make_trian_Dataset(name2originData=dataName, sample_opt=option2sample)
+    make_test_Dataset(name2originData=dataName, sample_opt=option2sample)
 
